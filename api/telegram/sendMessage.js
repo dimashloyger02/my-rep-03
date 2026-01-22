645082d4c856fb53cb30e248e169b80018574aaa
dimashloyger02 <94590260+dimashloyger02@users.noreply.github.com>
2026-01-22 22:50:00 +0700

    Update sendMessage.js

--- api/telegram/sendMessage.js
+++ api/telegram/sendMessage.js
@@ -1,18 +1,14 @@
-// api/telegram/sendMessage.js
 const axios = require('axios');
-
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
 module.exports = async (req, res) => {
     try {
         const { chat_id, text } = req.query;
         
-        // Валидация параметров
         if (!chat_id || !text) {
             return res.status(400).json({ error: 'Неверные параметры' });
         }
 
-        // Отправка запроса через прокси
         const response = await axios.post(
             `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
             {
