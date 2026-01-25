0171fae73e82eca94caab7f84d5ad041abaccfdc
dimashloyger02 <94590260+dimashloyger02@users.noreply.github.com>
2026-01-25 16:42:09 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ let currentFormat = true; // Формат отображения цен
 let nftData = []; // Массив данных NFT
 
 // URL для API
-const API_URL = 'https://gist.githubusercontent.com/dimashloyger02/8b284087c457766cde58c64507cc9af3/raw/b45a3535a1471a86838ccc1095e2aa5f96619db1/nft-data.json';
+const API_URL = 'https://gist.githubusercontent.com/dimashloyger02/8b284087c457766cde58c64507cc9af3/raw/d0d42f9b7dee5e0d99b35174c673ce9998f3b978/nft-data.json';
 
 // Модифицированная функция загрузки с индикатором
 function loadData() {
